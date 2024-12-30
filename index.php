40d569cedde56bd913511c9278403581dff4a19a
edumach <59918768+edumach@users.noreply.github.com>
2024-12-30 19:58:11 +0100

    Update index.php
    změna cesty k favicon.ico

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
       <title>Výukový web | <?php echo $_SERVER['HTTP_HOST'] ;?></title>
 	  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <link rel="shortcut icon" href="img/favicon.ico">
+      <link rel="shortcut icon" href="favicon.ico">
    </head>
    <body>	
    		<div class="container">
